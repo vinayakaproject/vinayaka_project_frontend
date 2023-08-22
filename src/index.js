7aa9b9855e496a7b3880f14004ef97bc1d2186ca
mohithworks <mohithworks2002@gmail.com>
2023-08-22 22:43:02 +0530

    Modified analytics 2

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react'
 
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-BRRM44PS00");
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -17,7 +20,13 @@ root.render(
   </React.StrictMode>
 );
 
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
--- src/useAnalyticsEventTracker.js
+++ src/useAnalyticsEventTracker.js
@@ -1,5 +1,5 @@
-import ReactGA from "react-ga";
 
+import ReactGA from "react-ga4";
 const useAnalyticsEventTracker = (category="Blog category") => {
     const eventTracker = (action = "test action", label = "test label") => {
         ReactGA.event({category, action, label});
